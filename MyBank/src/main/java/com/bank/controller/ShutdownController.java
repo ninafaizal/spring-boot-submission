be92b50e9023447af8efc0874734b8a710119926
ninafaizal <ninafaizal@outlook.my>
2025-08-04 09:28:46 +0800

    update shutdown controller

--- MyBank/src/main/java/com/bank/controller/ShutdownController.java
+++ MyBank/src/main/java/com/bank/controller/ShutdownController.java
@@ -4,23 +4,32 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShutdownController implements ApplicationContextAware {
-    
+
     private ApplicationContext context;
-    
-    @GetMapping("/shutdownContext")
-    public void shutdownContext() {
-        ((ConfigurableApplicationContext) context).close();
+
+    @PostMapping("/shutdownContext")
+    public String shutdownContext() {
+        Thread shutdownThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000); // Wait for 1 second to allow response to return
+                ((ConfigurableApplicationContext) context).close();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        shutdownThread.setDaemon(false);
+        shutdownThread.start();
+        return "Shutting down gracefully...";
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
-        this.context = ctx;
-        
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
     }
 }
